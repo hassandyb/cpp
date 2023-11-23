d2bada782e3eb846a744a64e43e39ff8071f48e9
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-11-23 12:30:13 +0100

    Add files via upload

--- Module_01/ex05/Harl.cpp
+++ Module_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 17:42:21 by hed-dyb           #+#    #+#             */
-/*   Updated: 2023/11/22 22:53:42 by hed-dyb          ###   ########.fr       */
+/*   Updated: 2023/11/23 12:26:49 by hed-dyb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,28 @@ Harl::~Harl() {};
 
 void Harl::debug( void )
 {
-	std::cout << "DEBUG: Says something" << std::endl;
+	std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 void Harl::info( void )
 {
-	std::cout << "INFO: Says something" << std::endl;
+	std::cout <<  "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 void Harl::warning( void )
 {
-	std::cout << "WARNING: Says something" << std::endl;
+	std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming forears whereas you started working here since last month." << std::endl;
 }
 void Harl::error( void )
 {
-	std::cout << "ERROR: Says something" << std::endl;
+	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 void Harl::complaining(std::string level)
 {
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	func_ptr ptrs[4] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	func_ptr deb = &Harl::debug;
+	func_ptr inf = &Harl::info;
+	func_ptr war = &Harl::warning;
+	func_ptr err = &Harl::error;
+	func_ptr ptrs[4] = { deb, inf, war, err};
 
 	for(int i = 0; i < 4; i++)
 	{
